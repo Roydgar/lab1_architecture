334b9e3b47ac886763f57dbf46c0ef099aa49d4c
Roydgar <Roydgaryshka@gmail.com>
2018-11-25 20:31:35 +0200

    refactor

--- Lab1.go
+++ Lab1.go
@@ -5,26 +5,24 @@ import (
 )
 
 func LevenshteinDistance(s1, s2 string) int {
-	min := func(values ...int) int {
-		m := values[0]
-		for _, v := range values {
-			if v < m {
-				m = v
-			}
-		}
-		return m
-	}
 	r1, r2 := []rune(s1), []rune(s2)
 	n, m := len(r1), len(r2)
 	if n > m {
 		r1, r2 = r2, r1
 		n, m = m, n
 	}
+
 	currentRow := make([]int, n+1)
 	previousRow := make([]int, n+1)
 	for i := range currentRow {
 		currentRow[i] = i
 	}
+
+	loop(m, currentRow, previousRow, r1, r2)
+	return currentRow[n]
+}
+
+func loop(m int, currentRow []int, previousRow []int, r1 []rune, r2 []rune) {
 	for i := 1; i <= m; i++ {
 		for j := range currentRow {
 			previousRow[j] = currentRow[j]
@@ -41,15 +39,23 @@ func LevenshteinDistance(s1, s2 string) int {
 			currentRow[j] = min(add, del, change)
 		}
 	}
-	return currentRow[n]
 }
 
-func SortArrayByLevenshteinDistance(array []string, keyword string) []string{
+func min(values ...int) int {
+	m := values[0]
+	for _, v := range values {
+		if v < m {
+			m = v
+		}
+	}
+	return m
+}
+
+func SortArrayByLevenshteinDistance(array []string, keyword string) []string {
 
 	sort.Slice(array, func(i, j int) bool {
-		return LevenshteinDistance(array[i], keyword) <  LevenshteinDistance(array[j], keyword)
+		return LevenshteinDistance(array[i], keyword) < LevenshteinDistance(array[j], keyword)
 	})
 
 	return array
 }
-
